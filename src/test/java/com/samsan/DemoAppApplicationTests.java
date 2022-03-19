9c3d8def825e857830ccfedd623718da8a0b902c
aditya <aditya@gmail.com>
2022-03-19 13:13:13 +0530

    first commit

---
+++ src/test/java/com/samsan/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
